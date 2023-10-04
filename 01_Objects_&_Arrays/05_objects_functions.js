5dd19cf084e7806154bb99ad3f09ad30c965436d
webfussel <fiona@webfussel.de>
2023-10-04 18:43:51 +0200

    ADD: Objects & Arrays advanced

--- 01_Objects_&_Arrays/05_objects_functions.js
+++ 01_Objects_&_Arrays/05_objects_functions.js
@@ -32,3 +32,44 @@ const hasDevilFruit = Object.hasOwn(luffy, 'devilFruit')
 const hasSwimmingSkill = Object.hasOwn(luffy, 'swimmingSkill')
 console.log('luffy - has devil fruit', hasDevilFruit)
 console.log('luffy - has swimming skill', hasSwimmingSkill)
+
+// Object.assign
+// Kopiert alle Schlüssel-Wert-Paare von einem Objekt in ein anderes.
+const objA = {
+    a: 1,
+    b: 2,
+}
+
+const objB = {
+    b: 3,
+    c: 4,
+}
+
+const objC = Object.assign(objA, objB)
+console.log('objA', objA)
+console.log('objB', objB)
+console.log('objC', objC)
+
+// Object.freeze
+// Verhindert, dass das Objekt verändert werden kann.
+const frozenLuffy = Object.freeze(luffy)
+// Auskommentiert, weil Fehler
+// frozenLuffy.age = 20
+console.log('frozenLuffy', frozenLuffy)
+
+// Object.seal
+// Verhindert, dass neue Schlüssel hinzugefügt oder bestehende Schlüssel gelöscht werden können.
+// Allerdings können die Werte der Schlüssel noch verändert werden.
+const sealedLuffy = Object.seal(luffy)
+sealedLuffy.bla = 'asdf'
+console.log('sealedLuffy', sealedLuffy)
+
+// Object.isFrozen
+// Gibt true zurück, wenn das Objekt eingefroren ist.
+const isFrozen = Object.isFrozen(frozenLuffy)
+console.log('isFrozen', isFrozen)
+
+// Object.isSealed
+// Gibt true zurück, wenn das Objekt versiegelt ist.
+const isSealed = Object.isSealed(sealedLuffy)
+console.log('isSealed', isSealed)
