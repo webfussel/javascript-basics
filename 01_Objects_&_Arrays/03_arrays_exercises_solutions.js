30245b3343af0b8562795253f91255294a030f25
webfussel <fiona@webfussel.de>
2023-10-05 14:52:14 +0200

    ADD: Solutions split from exercises

--- 01_Objects_&_Arrays/03_arrays_exercises_solutions.js
+++ 01_Objects_&_Arrays/03_arrays_exercises_solutions.js
@@ -23,19 +23,32 @@ console.log(strawHatPirates)
 
 // Aufgabe 1
 // Gib den Namen des Kapitäns der Strohhutpiraten aus. (Für alle nicht-weebs: Es ist Ruffy)
+console.log('Aufgabe 1:', strawHatPirates[0])
 
 // Aufgabe 2
 // Sortiere die Crewmitglieder umgekehrt alphabetisch und gib sie aus.
+const task2 = strawHatPirates.toSorted().toReversed()
+console.log('Aufgabe 2:', task2)
 
 // Aufgabe 3
 // Gib die Namen der ersten drei Crewmitglieder nach dem Sortieren aus.
+const task3 = []
+for (let i = 0; i < 3; i++) {
+    task3.push(task2[i])
+}
+console.log('Aufgabe 3:', task3)
 
 // Aufgabe 4
 // Filtere alle Crewmitglieder aus, die nur einen Namen haben, sodass nur die mit mehreren Namen übrig bleiben.
+const task4 = strawHatPirates.filter(element => element.includes(' '))
+console.log('Aufgabe 4:', task4)
 
 // Aufgabe 5
 // Gib die Namen aller Crewmitglieder aus, die mehr als 8 Buchstaben in ihrem Namen haben.
-
+const task5 = strawHatPirates.filter(element => element.length > 8)
+console.log('Aufgabe 5:', task5)
 
 // Aufgabe 6
 // Erstelle programmatisch ein Array, dass die Anzahl der Zeichen in den Namen der Crewmitglieder enthält.
+const task6 = strawHatPirates.map(element => element.length)
+console.log('Aufgabe 6:', task6)
