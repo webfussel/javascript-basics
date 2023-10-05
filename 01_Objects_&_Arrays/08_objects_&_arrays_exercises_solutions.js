12ce9b7dc5a77af046eb083bdfe3e219373ef6c8
webfussel <fiona@webfussel.de>
2023-10-05 21:27:21 +0200

    ADD: Addition for destructuring

--- 01_Objects_&_Arrays/08_objects_&_arrays_exercises_solutions.js
+++ 01_Objects_&_Arrays/08_objects_&_arrays_exercises_solutions.js
@@ -47,20 +47,33 @@ const warLordsOfTheSea = [
 
 // Aufgabe 1:
 // Erstelle eine Funktion, die ein Array von Objekten entgegennimmt und die Namen der Personen ausgibt.
-
+const task1 = arr => arr?.forEach(element => console.log('Aufgabe 1:', element.name))
+task1(warLordsOfTheSea)
 
 // Aufgabe 2:
 // Erstelle eine Funktion, die ein Array von Objekten entgegennimmt und die Namen der Personen ausgibt, die eine Teufelsfrucht besitzen.
+const task2 = arr => arr?.filter(element => element.devilFruit !== 'unknown').forEach(element => console.log('Aufgabe 2:', element.name))
+task2(warLordsOfTheSea)
 
 // Aufgabe 3:
 // Filtere alle Personen aus, die keine Teufelsfrucht haben, sodass nur noch Personen mit Teufelsfrucht im neuen Array sind.
+const task3 = warLordsOfTheSea.filter(element => element.devilFruit !== 'unknown')
+console.log('Aufgabe 3:', task3)
 
 // Aufgabe 4:
 // Sortiere das Array nach der Höhe des Kopfgelds, absteigend.
+const warlordsCopy = [...warLordsOfTheSea]
+warlordsCopy.sort((a,b) => {
+    const valA = a.exBounty === 'unknown' || a.exBounty === null ? 0 : a.exBounty
+    const valB = b.exBounty === 'unknown' || b.exBounty === null ? 0 : b.exBounty
+
+    return valB - valA
+})
+console.log('Aufgabe 4:', warlordsCopy)
 
 // Aufgabe 5:
 // Das Array ist zu unleserlich. Die Weltregierung braucht eine simple übersicht mit dem Namen der Person und der Höhe des Kopfgelds.
-// Wenn das Kopfgeld unbekannt ist, soll es durch 0 ersetzt werden.
+// Wenn das Kopfgeld unbekannt oder null ist, soll es durch 0 ersetzt werden.
 // Beispiel:
 // {
 //     'Mihawk Dulacre Falkenauge': 0,
@@ -69,12 +82,22 @@ const warLordsOfTheSea = [
 //     ...
 // }
 // Das Objekt soll programmatisch erstellt werden. NICHT PER HAND!
+const task5 = warLordsOfTheSea.reduce((result, current) => {
+    const key = current.name
+    const value = current.exBounty === 'unknown' || current.exBounty === null ? 0 : current.exBounty
+
+    result[key] = value
+    return result
+}, {})
+console.log('Aufgabe 5:', task5)
 
 // Aufgabe 6:
 // Vier Samurai wurden entweder entfernt, getötet oder sind ausgetreten.
 // Lösche Doflamingo, Gecko Moria, Jimbei und Blackbeard.
 // (Verändere das vorhandene Array. Erstelle kein neues.)
-
+warLordsOfTheSea.splice(1, 2)
+warLordsOfTheSea.splice(2, 2)
+console.log('Aufgabe 6:', warLordsOfTheSea)
 
 // Aufgabe 7:
 // Vor ihrer Auflösung bestanden die Samurai noch aus 5 Mitgliedern.
@@ -84,9 +107,40 @@ const warLordsOfTheSea = [
 // Erstelle eine neue Variable 'lastKnownWarlords' und weise ihr das neue Array zu.
 // Buggy | 'The Clown' | 15_000_000 | Bara Bara Fruit
 // Edward Weevil | 'Whitebeard Jr.' | 480_000_000 | unknown
+const lastKnownWarlords = [...warLordsOfTheSea, {
+    name: 'Buggy',
+    description: 'The Clown',
+    exBounty: 15_000_000,
+    devilFruit: 'Bara Bara Fruit',
+}, {
+    name: 'Edward Weevil',
+    description: 'Whitebeard Jr',
+    exBounty: 480_000_000,
+    devilFruit: 'unkown',
+}]
+console.log('Aufgabe 7:', lastKnownWarlords)
 
 // Aufgabe 8:
 // Sortiere das Array noch einmal, diesmal nach dem Namen, alphabetisch.
 // Ziehe dann den ersten und den letzten Eintrag aus dem Array.
 // Gib die Namen der beiden Personen aus.
 // Nutze nicht die pop- oder shift-Methoden und auch nicht den direkten Zugriff auf das Array per Index.
+lastKnownWarlords.sort((a, b) => {
+    const valA = a.name
+    const valB = b.name
+
+    if (valA < valB) {
+        return -1
+    }
+
+    if (valB < valA) {
+        return 1
+    }
+
+    return 0
+})
+
+const [first,,,,last] = lastKnownWarlords
+
+console.log('Aufgabe 8:', first, last)
+console.log('Aufgabe 8:', lastKnownWarlords)
