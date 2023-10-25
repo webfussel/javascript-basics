f98375984aac5e6649895402221abd25105dcdbe
webfussel <fiona@webfussel.de>
2023-10-25 18:42:55 +0200

    ADD: better Promise stuff

---
+++ 04_Libraries_&_APIs/02_Promises_exercises.js
@@ -0,0 +1,19 @@
+console.log('Promises - Exercises')
+
+// Aufgabe 1: Erstelle eine Funktion, die ein Promise zurückgibt.
+// Die Funktion soll eine Zahl entgegen nehmen.
+// Diese Zahl stellt die Zeit in Sekunden dar, die vergehen soll, bis das Promise erfüllt wird.
+// Das Promise soll dabei einen String zurückgeben, der ein zufälliges Passwort enthält.
+
+
+// Aufgabe 2: Erstelle eine Funktion, die ein Promise zurückgibt.
+// Die Funktion soll einen String entgegen nehmen.
+// Wenn der String ein valides Passwort ist ist, soll das Promise erfüllt werden.
+// Passwortvorgaben: Mindestens 8 Zeichen, mindestens ein Großbuchstabe, mindestens eine Zahl, mindestens ein Kleinbuchstabe
+
+
+// Aufgabe 3: Lass beide Promises parallel laufen und gib die Ergebnisse in der Konsole aus.
+
+
+// Aufgabe 4: Lass beide Promises nacheinander laufen
+// Übergebe das Zufallspasswort von Aufgabe 1 an die Funktion von Aufgabe 2 und lass es so überprüfen.
