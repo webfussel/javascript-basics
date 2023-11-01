4405827af091edea0c880ebe5fea779653e83b48
webfussel <fiona@webfussel.de>
2023-11-01 08:55:20 +0100

    ADD: Übungen

---
+++ 04_Libraries_&_APIs/08_Exercises_Observers_Imports_Exports/index.js
@@ -0,0 +1,28 @@
+// Aufgabe 0
+// Erstelle eine HTML Datei mit 3 Abschnitten, deren Abschnitte so groß wie die Viewport Höhe sind.
+// Zusätzlich sollen dort kleinere Kästen mit irgendwelchen Farben und Größen drin sein.
+
+
+// Aufgabe 1
+// Erstelle einen Observer, der die Abschnitte beobachtet und bei jedem Abschnittswechsel den Namen des Abschnitts in die Konsole schreibt.
+
+
+// Aufgabe 2
+// Erstelle eine Klasse, die einen Observer erstellt und die Abschnitte beobachtet.
+// Die Klasse soll sich in einer eigenen Datei befinden
+
+
+// Aufgabe 3
+// Erstelle einen Observer, der die kleinen Elemente in euren Abschnitten beobachtet und sie bei 70% Sichtbarkeit zu einer anderen Farbe ändert.
+
+
+// Aufgabe 4
+// Sobald die Farbe geändert wurde, soll eine Zufällige Zahl pro Element generiert werden.
+// Diese Zahl soll in das jeweilige Element hineingeschrieben werden.
+// Erstelle einen Observer, der dies Beobachtet und die generierte Zahl mit 42 addiert und in die Konsole schreibt.
+
+
+// Aufgabe 5
+// Erstelle einen Observer, der die Abschnitte beobachtet.
+// Er soll die Helligkeit der Hintergrundfarbe des Abschnitts in Abhängigkeit seiner Breite ändern.
+// Welche Farben ihr nutzt und in welchen Abständen diese geändert wird und auch ob dies graduell oder abrupt geschieht, ist euch überlassen.
